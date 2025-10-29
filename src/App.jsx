640da8b11d3b4fd9b42eecc4ae6e0702a1697be1
hnardi38-rgb <hnardi38@students.kusd.edu>
2025-10-29 00:55:18 -0500

    making npm stuff work for me

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import About from './components/pages/About'
 import Contact from './components/pages/Contact'
+import Officers from './components/pages/Officers'
+import Events from './components/pages/Events'
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
         <Route path="/" element={<h1>Home</h1>} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/officers" element={<Officers />} />
       </Routes>
       </main>
     </BrowserRouter>
