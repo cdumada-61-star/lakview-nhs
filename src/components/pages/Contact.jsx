9f49df13db5fa79e204eba9934a941054f8d6d9c
cdumada-61-star <cdumada-61@students.kusd.edu>
2025-10-29 19:28:21 -0500

    Update Contact.jsx

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -40,15 +40,14 @@ function Contact() {
   };
 
   return (
-    <main className="page-section" id="contact" style={{ paddingTop: "50px"}}>
+    <main className="page-section" id="contact" style={{ paddingTop: "25px"}}>
       <div className="container px-4 px-lg-5">
         <div className="row gx-4 gx-lg-5 justify-content-center">
           <div className="col-lg-8 col-xl-6 text-center">
             <h2 className="mt-0">Let's Get In Touch!</h2>
             <hr className="divider" />
             <p className="text-muted mb-5">
-              Ready to start your next project with us? Send us a message and we
-              will get back to you as soon as possible!
+              Contact us with this form to reach out about any questions or if you're interested in joining NHS!
             </p>
           </div>
         </div>
